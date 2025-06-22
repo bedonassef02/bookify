6726feade84e04f2052228a3fa4779ac870a556e
Bedo <bedonassef71@gmail.com>
2025-06-23 02:51:22 +0300

    api-gateway > common > interceptors > +rpc-exception (to convert to Http Exceptions)

--- apps/api-gateway/src/app.module.ts
+++ apps/api-gateway/src/app.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
 import { EventsModule } from './events/events.module';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { RpcExceptionInterceptor } from './common/interceptors/rpc-exception.interceptor';
 
 @Module({
   imports: [EventsModule],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: RpcExceptionInterceptor,
+    },
+  ],
 })
 export class AppModule {}
