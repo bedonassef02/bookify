7bf0e8fd963bbf8186b6fe91fa88131a3713d66d
Bedo <bedonassef71@gmail.com>
2025-06-23 02:01:45 +0300

    event-service > service > implement create new event

--- apps/event-service/src/event.controller.ts
+++ apps/event-service/src/event.controller.ts
@@ -1,18 +1,25 @@
 import { Controller } from '@nestjs/common';
 import { EventService } from './event.service';
 import { MessagePattern } from '@nestjs/microservices';
+import { CreateEventDto } from './dto/create-event.dto';
+import { Event } from './entities/event.entity';
 
 @Controller()
 export class EventController {
   constructor(private readonly eventService: EventService) {}
 
   @MessagePattern({ cmd: 'events.findAll' })
-  findAll(): [] {
+  async findAll(): Promise<Event[]> {
     return this.eventService.findAll();
   }
 
   @MessagePattern({ cmd: 'events.findOne' })
-  findOne(data: { id: string }): string {
+  async findOne(data: { id: string }): Promise<Event | null> {
     return this.eventService.findOne(data.id);
+  }
+
+  @MessagePattern({ cmd: 'events.create' })
+  async create(createEventDto: CreateEventDto): Promise<Event> {
+    return this.eventService.create(createEventDto);
   }
 }
--- apps/event-service/src/event.service.ts
+++ apps/event-service/src/event.service.ts
@@ -1,12 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Event, EventDocument } from './entities/event.entity';
+import { CreateEventDto } from './dto/create-event.dto';
 
 @Injectable()
 export class EventService {
-  findAll(): [] {
-    return [];
+  constructor(
+    @InjectModel(Event.name) private eventModel: Model<EventDocument>,
+  ) {}
+
+  async create(createEventDto: CreateEventDto): Promise<Event> {
+    return this.eventModel.create(createEventDto);
   }
 
-  findOne(id: string): string {
-    return `Event #${id}`;
+  async findAll(): Promise<Event[]> {
+    return this.eventModel.find().exec();
+  }
+
+  async findOne(id: string): Promise<Event | null> {
+    return this.eventModel.findById(id).exec();
   }
 }
