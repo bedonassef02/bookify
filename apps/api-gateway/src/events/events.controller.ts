c892e192647eeed5e569be255f2ffcaa32d999d9
Bedo <bedonassef71@gmail.com>
2025-06-23 02:02:17 +0300

    api-gateway > events > controller > +create api endpoint

--- apps/api-gateway/src/events/events.controller.ts
+++ apps/api-gateway/src/events/events.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Get, Inject, Param } from '@nestjs/common';
+import { Body, Controller, Get, Inject, Param, Post } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
+import { CreateEventDto } from '../../../event-service/src/dto/create-event.dto';
+import { Observable } from 'rxjs';
 
 @Controller('events')
 export class EventsController {
@@ -11,7 +13,12 @@ export class EventsController {
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  findOne(@Param('id') id: string): Observable<Event | null> {
     return this.client.send({ cmd: 'events.findOne' }, { id });
+  }
+
+  @Post()
+  create(@Body() createEventDto: CreateEventDto) {
+    return this.client.send({ cmd: 'events.create' }, createEventDto);
   }
 }
