de499459937609b879f17d5e25c090ee3040c6f0
Bedo <bedonassef71@gmail.com>
2025-06-25 01:24:58 +0300

    api-gateway > common > interceptors > logging > log response status code and execution time

--- apps/api-gateway/src/common/interceptors/logging.interceptor.ts
+++ apps/api-gateway/src/common/interceptors/logging.interceptor.ts
@@ -6,13 +6,25 @@ import {
   NestInterceptor,
 } from '@nestjs/common';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 @Injectable()
 export class LoggingInterceptor implements NestInterceptor {
   private readonly logger = new Logger(LoggingInterceptor.name);
+
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     const request = context.switchToHttp().getRequest();
-    this.logger.debug(`[${request.method}] ${request.url}`);
-    return next.handle();
+    const response = context.switchToHttp().getResponse();
+    const now = Date.now();
+
+    return next.handle().pipe(
+      tap(() => {
+        const statusCode = response.statusCode;
+        const elapsedTime = Date.now() - now;
+        this.logger.debug(
+          `[${request.method}] ${request.url} - [${statusCode}] ${elapsedTime}ms`,
+        );
+      }),
+    );
   }
 }
