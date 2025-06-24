b6025e408f1b0eb3ae94f4a338d8ab27a3cb80a6
Bedo <bedonassef71@gmail.com>
2025-06-25 00:23:36 +0300

    api-gateway > users > auth > decorators > +public & roles

--- apps/api-gateway/src/users/users.controller.ts
+++ apps/api-gateway/src/users/users.controller.ts
@@ -2,16 +2,19 @@ import { Body, Controller, Inject, Post } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 import { AuthResponse, PATTERNS, SignInDto, SignUpDto } from '@app/shared';
 import { Observable } from 'rxjs';
+import { Public } from './auth/decorators/public.decorator';
 
 @Controller('users')
 export class UsersController {
   constructor(@Inject('USER_SERVICE') private client: ClientProxy) {}
 
+  @Public()
   @Post('sign-up')
   signUp(@Body() signUpDto: SignUpDto): Observable<AuthResponse> {
     return this.client.send(PATTERNS.USERS.SIGN_UP, signUpDto);
   }
 
+  @Public()
   @Post('sign-in')
   signIn(@Body() signInDto: SignInDto): Observable<AuthResponse> {
     return this.client.send(PATTERNS.USERS.SIGN_IN, signInDto);
