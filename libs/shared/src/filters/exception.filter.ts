c8ddd6fc3e79f36fbd2de95569abce3fac273cd6
Bedo <bedonassef71@gmail.com>
2025-07-07 15:30:49 +0300

    libs > shared > filters > exception > skip Rpc Exceptions

--- libs/shared/src/filters/exception.filter.ts
+++ libs/shared/src/filters/exception.filter.ts
@@ -1,10 +1,16 @@
-import { Catch, RpcExceptionFilter, ArgumentsHost } from '@nestjs/common';
-import { Observable, throwError } from 'rxjs';
+import {
+  Catch,
+  ArgumentsHost,
+  ExceptionFilter as NestExceptionFilter,
+} from '@nestjs/common';
+import { throwError } from 'rxjs';
 import { RpcException } from '@nestjs/microservices';
 
 @Catch()
-export class ExceptionFilter implements RpcExceptionFilter<RpcException> {
-  catch(exception: RpcException, host: ArgumentsHost): Observable<any> {
-    return throwError(() => exception);
+export class ExceptionFilter implements NestExceptionFilter {
+  catch(exception: Error, host: ArgumentsHost) {
+    if (!(exception instanceof RpcException)) {
+      return throwError(() => exception);
+    }
   }
 }
