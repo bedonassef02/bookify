a9de4082e47c7d41846d40fa1bfee050fd0c7d88
Bedo <bedonassef71@gmail.com>
2025-07-10 14:45:52 +0300

    api-gateway > users > auth > decorators > current-user > return null for unauthenticated requests

--- apps/api-gateway/src/users/auth/decorators/current-user.decorator.ts
+++ apps/api-gateway/src/users/auth/decorators/current-user.decorator.ts
@@ -3,6 +3,10 @@ import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 export const CurrentUser = createParamDecorator(
   (data: undefined | string, ctx: ExecutionContext) => {
     const request = ctx.switchToHttp().getRequest();
+    if (!request.user) {
+      return null;
+    }
+
     return !data ? request.user : request.user[data];
   },
 );
