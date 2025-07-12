70b14f50e4bed8c58dded13efce42417ac5fd99f
Bedo <bedonassef71@gmail.com>
2025-07-12 15:50:36 +0300

    api-gateway > main.ts > remove validation options

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -5,13 +5,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      transform: true,
-      forbidNonWhitelisted: true,
-    }),
-  );
+  app.useGlobalPipes(new ValidationPipe());
 
   const config = new DocumentBuilder()
     .setTitle('Events API')
