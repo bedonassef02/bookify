57145a689515933e9502504fa28161f57c375ec4
Bedo <bedonassef71@gmail.com>
2025-07-15 14:12:34 +0300

    notification-service > entities > user-preference > remove $useSms

--- apps/notification-service/src/entities/user-preference.entity.ts
+++ apps/notification-service/src/entities/user-preference.entity.ts
@@ -1,17 +1,12 @@
 import { Prop, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
 
-export type UserPreferenceDocument = UserPreference & Document;
-
-export class UserPreference {
+export class UserPreference extends Document {
   @Prop({ unique: true })
   email: string;
 
   @Prop({ type: Boolean, default: false })
   useEmail: boolean;
-
-  @Prop({ type: Boolean, default: false })
-  useSms: boolean;
 
   @Prop({ type: Boolean, default: true })
   usePush: boolean;
