4b9a167bd2f348512288bf536afa80868a64aa81
Bedo <bedonassef71@gmail.com>
2025-07-20 12:54:46 +0300

    api-gateway > common > file > services > +Cloudinary

---
+++ apps/api-gateway/src/common/files/services/cloudinary.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+import { v2 as cloudinary } from 'cloudinary';
+import { CloudinaryResponse } from './cloudinary-response';
+import * as streamifier from 'streamifier';
+
+@Injectable()
+export class CloudinaryService {
+  uploadFile(file: Express.Multer.File): Promise<CloudinaryResponse> {
+    return new Promise<CloudinaryResponse>((resolve, reject) => {
+      const uploadStream = cloudinary.uploader.upload_stream(
+        (error, result) => {
+          if (error) return reject(error);
+          if (!result) {
+            return reject(new Error('Upload result is undefined'));
+          }
+          resolve(result);
+        },
+      );
+
+      streamifier.createReadStream(file.buffer).pipe(uploadStream);
+    });
+  }
+}
