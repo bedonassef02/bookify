82d8db29b5c92bd4035f6928c7722956363c4c1c
Bedo <bedonassef71@gmail.com>
2025-07-22 02:21:31 +0300

    user-service > seed > user > Improve user seeding logic

--- apps/user-service/src/seed/user.seeder.ts
+++ apps/user-service/src/seed/user.seeder.ts
@@ -15,49 +15,37 @@ export class UserSeeder {
     const usersToSeed: CreateUserDto[] = [];
 
     for (let i = 0; i < count; i++) {
-      const firstName = faker.person.firstName();
-      const lastName = faker.person.lastName();
-      const email = faker.internet.email({ firstName, lastName });
-      const password = 'password123';
-      const phoneNumber = faker.phone.number();
-      const role = faker.helpers.arrayElement([
-        Role.USER,
-        Role.USER,
-        Role.USER,
-        Role.ADMIN,
-      ]);
-
-      usersToSeed.push({
-        email,
-        password,
-        firstName,
-        lastName,
-        phoneNumber,
-        role,
-      });
+      usersToSeed.push(this.createUserDto());
     }
 
     for (const userData of usersToSeed) {
       try {
-        const existingUser = await this.userService.findByEmail(userData.email);
-        if (existingUser) {
-          console.log(
-            `User with email ${userData.email} already exists. Skipping.`,
-          );
-          continue;
-        }
+        await this.isExist(userData.email);
 
-        const hashedPassword = await this.hashingService.hash(
-          userData.password,
-        );
-        await this.userService.create({
-          ...userData,
-          password: hashedPassword,
-        });
+        const password = await this.hashingService.hash(userData.password);
+        await this.userService.create({ ...userData, password });
         console.log(`User ${userData.email} seeded successfully.`);
       } catch (error) {
         console.error(`Error seeding user ${userData.email}:`, error.message);
       }
+    }
+  }
+
+  private createUserDto(): CreateUserDto {
+    return {
+      email: faker.internet.email(),
+      password: 'password123',
+      firstName: faker.person.firstName(),
+      lastName: faker.person.lastName(),
+      phoneNumber: faker.phone.number(),
+      role: Role.USER,
+    };
+  }
+
+  private async isExist(email: string) {
+    const user = await this.userService.findByEmail(email);
+    if (user) {
+      console.log(`User with email ${email} already exists. Skipping.`);
     }
   }
 }
