f1674612b84a36ce409904c178b677fe41c5f5df
Bedo <bedonassef71@gmail.com>
2025-07-21 17:58:20 +0300

    booking-service > services > ticket-tier.service > throw error if ticket-tier not found

--- apps/booking-service/src/services/ticket-tier.service.ts
+++ apps/booking-service/src/services/ticket-tier.service.ts
@@ -1,16 +1,26 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 import { firstValueFrom } from 'rxjs';
-import { EVENT_SERVICE, Patterns, TicketTierType } from '@app/shared';
+import {
+  EVENT_SERVICE,
+  Patterns,
+  RpcNotFoundException,
+  TicketTierType,
+} from '@app/shared';
 
 @Injectable()
 export class TicketTierService {
   constructor(@Inject(EVENT_SERVICE) private eventService: ClientProxy) {}
 
-  findOne(id: string): Promise<TicketTierType> {
-    return firstValueFrom(
+  async findOne(id: string): Promise<TicketTierType> {
+    const ticketTier: TicketTierType | null = await firstValueFrom(
       this.eventService.send(Patterns.TICKET_TIERS.FIND_ONE, { id }),
     );
+    if (!ticketTier) {
+      throw new RpcNotFoundException('Ticket tier not found');
+    }
+
+    return ticketTier;
   }
 
   updateBookedSeats(id: string, increment: number): void {
