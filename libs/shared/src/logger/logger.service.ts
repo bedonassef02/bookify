b47d75505592b6e2d9714d1298889d359ab48af5
Bedo <bedonassef71@gmail.com>
2025-07-25 16:51:00 +0300

    shared > logger > update: to extends ConsoleLogger

--- libs/shared/src/logger/logger.service.ts
+++ libs/shared/src/logger/logger.service.ts
@@ -1,8 +1,8 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { Injectable, ConsoleLogger } from '@nestjs/common';
 import * as winston from 'winston';
 
 @Injectable()
-export class LoggerService extends Logger {
+export class LoggerService extends ConsoleLogger {
   private readonly logger: winston.Logger;
 
   constructor() {
