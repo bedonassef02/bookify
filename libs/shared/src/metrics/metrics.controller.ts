7d6167ec5eceff212eccd0813ddddf16d723335a
Bedo <bedonassef71@gmail.com>
2025-07-26 17:13:12 +0300

    shared > metrics > controller > make endpoint is public

--- libs/shared/src/metrics/metrics.controller.ts
+++ libs/shared/src/metrics/metrics.controller.ts
@@ -1,9 +1,11 @@
 import { Controller, Get, Res } from '@nestjs/common';
 import { Response } from 'express';
 import * as client from 'prom-client';
+import { Public } from '../../../../apps/api-gateway/src/users/auth/decorators/public.decorator';
 
 @Controller('metrics')
 export class MetricsController {
+  @Public()
   @Get()
   async getMetrics(@Res() res: Response) {
     res.set('Content-Type', client.register.contentType);
