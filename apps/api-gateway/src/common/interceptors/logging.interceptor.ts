ec4744fb1a2730477e1ad7bad31fffee06d9c4a3
Bedo <bedonassef71@gmail.com>
2025-07-26 17:19:53 +0300

    shared > interceptors > logging > use metrics service

--- apps/api-gateway/src/common/interceptors/logging.interceptor.ts
+++ apps/api-gateway/src/common/interceptors/logging.interceptor.ts
@@ -7,31 +7,57 @@ import {
 import { Observable } from 'rxjs';
 import { catchError, tap } from 'rxjs/operators';
 import { Request, Response } from 'express';
-import { LoggerService } from '@app/shared';
+import { LoggerService } from '@app/shared/logger/logger.service';
+import { MetricsService } from '@app/shared/metrics/metrics.service';
 
 @Injectable()
 export class LoggingInterceptor implements NestInterceptor {
-  private readonly logger: LoggerService = new LoggerService();
+  constructor(
+    private readonly logger: LoggerService,
+    private readonly metricsService: MetricsService,
+  ) {}
 
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     const request = context.switchToHttp().getRequest<Request>();
     const response = context.switchToHttp().getResponse<Response>();
     const now = Date.now();
 
-    const statusCode = response.statusCode;
     return next.handle().pipe(
       tap(() => {
         const elapsedTime = Date.now() - now;
-        this.logger.debug(
-          `[${request.method}] ${request.url} - [${statusCode}] ${elapsedTime}ms`,
+        const { method, url } = request;
+        const { statusCode } = response;
+
+        const message = `[${method}] ${url} - ${statusCode} - ${elapsedTime}ms`;
+        this.logger.log(message);
+
+        this.metricsService.httpRequestDuration.observe(
+          {
+            method,
+            route: url,
+            code: statusCode,
+          },
+          elapsedTime / 1000,
         );
       }),
       catchError((error) => {
         const elapsedTime = Date.now() - now;
-        this.logger.error(
-          `[${request.method}] ${request.url} - [${statusCode}] ${elapsedTime}ms`,
-        );
+        const { method, url } = request;
+        const { statusCode } = response;
+
+        const message = `[${method}] ${url} - ${statusCode} - ${elapsedTime}ms`;
+        this.logger.error(message);
         this.logger.error(JSON.stringify(error));
+
+        this.metricsService.httpRequestDuration.observe(
+          {
+            method,
+            route: url,
+            code: statusCode,
+          },
+          elapsedTime / 1000,
+        );
+
         throw error;
       }),
     );
