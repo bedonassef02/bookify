d0d1dbe4865a4b37e3df49b9de1017d787ea6496
Bedo <bedonassef71@gmail.com>
2025-07-26 16:48:37 +0300

    logging.interceptor > use our custom LoggerService

--- apps/api-gateway/src/common/interceptors/logging.interceptor.ts
+++ apps/api-gateway/src/common/interceptors/logging.interceptor.ts
@@ -2,16 +2,16 @@ import {
   CallHandler,
   ExecutionContext,
   Injectable,
-  Logger,
   NestInterceptor,
 } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { catchError, tap } from 'rxjs/operators';
 import { Request, Response } from 'express';
+import { LoggerService } from '@app/shared';
 
 @Injectable()
 export class LoggingInterceptor implements NestInterceptor {
-  private readonly logger = new Logger(LoggingInterceptor.name);
+  private readonly logger: LoggerService = new LoggerService();
 
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     const request = context.switchToHttp().getRequest<Request>();
--- libs/shared/src/interceptors/logging.interceptor.ts
+++ libs/shared/src/interceptors/logging.interceptor.ts
@@ -2,17 +2,16 @@ import {
   CallHandler,
   ExecutionContext,
   Injectable,
-  Logger,
   NestInterceptor,
 } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { catchError, tap } from 'rxjs/operators';
 import { RmqContext } from '@nestjs/microservices';
-import { Redactor } from '@app/shared';
+import { Redactor, LoggerService } from '@app/shared';
 
 @Injectable()
 export class LoggingInterceptor implements NestInterceptor {
-  private readonly logger = new Logger(LoggingInterceptor.name);
+  private readonly logger: LoggerService = new LoggerService();
 
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     const rpcContext = context.switchToRpc();
