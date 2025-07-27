124e5d89aecc3a38c0c8c10a415ffdc8f2cc743a
Bedo <bedonassef71@gmail.com>
2025-07-28 01:51:59 +0300

    review-service > remove deprecated logger

--- apps/review-service/src/main.ts
+++ apps/review-service/src/main.ts
@@ -1,13 +1,9 @@
 import { AppModule } from './app.module';
-import {
-  RmqMicroserviceBootstrap,
-  REVIEW_QUEUE,
-  LoggerService,
-} from '@app/shared';
+import { RmqMicroserviceBootstrap, REVIEW_QUEUE } from '@app/shared';
 
 async function bootstrap() {
   const app = await RmqMicroserviceBootstrap(AppModule, REVIEW_QUEUE);
-  app.useLogger(app.get(LoggerService));
+
   await app.listen();
 }
 bootstrap();
